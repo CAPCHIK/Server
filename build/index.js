c5d1630fb6ea928a263313a235e7edfe16258a9a
Maksim <maksalmak@gmail.com>
2018-01-28 03:48:34 +0300

    Init

---
+++ build/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http = require("http");
+const sockio = require("socket.io");
+const server = http.createServer((req, res) => {
+    res.end("For replication");
+});
+server.listen(4000);
+const io = sockio(server);
+io.on("connection", socket => {
+    console.log("added client");
+    socket.on("freeze", data => socket.broadcast.emit("freeze", data));
+    socket.on("treasure_hp", () => socket.broadcast.emit("treasure_hp"));
+    socket.on("build_tower", data => socket.broadcast.emit("build_tower", data));
+    socket.on("carry_buff", () => {
+        console.log('carry buff');
+        socket.broadcast.emit("carry_buff");
+    });
+    socket.on("move_treasure", data => socket.broadcast.emit("move_treasure", data));
+});
+//# sourceMappingURL=index.js.map
