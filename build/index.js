1518192a67bc6ed44f109bb145230a81c8fc883d
Maksim <maksalmak@gmail.com>
2018-01-28 09:35:07 +0300

    add new message types

--- build/index.js
+++ build/index.js
@@ -5,15 +5,18 @@ const sockio = require("socket.io");
 const server = http.createServer((req, res) => {
     res.end("For replication");
 });
-server.listen(4000);
+server.listen(80);
 const io = sockio(server);
 io.on("connection", socket => {
     console.log("added client");
     socket.on("freeze", data => socket.broadcast.emit("freeze", data));
     socket.on("treasure_hp", () => socket.broadcast.emit("treasure_hp"));
     socket.on("build_tower", data => socket.broadcast.emit("build_tower", data));
+    socket.on("playerUpdate", data => socket.broadcast.emit("playerUpdate", data));
+    socket.on("treasureUpdate", data => socket.broadcast.emit("treasureUpdate", data));
+    socket.on("mobsUpdate", data => socket.broadcast.emit("mobsUpdate", data));
+    socket.on("lootUpdate", data => socket.broadcast.emit("lootUpdate", data));
     socket.on("carry_buff", () => {
-        console.log('carry buff');
         socket.broadcast.emit("carry_buff");
     });
     socket.on("move_treasure", data => socket.broadcast.emit("move_treasure", data));
